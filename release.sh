126a02920c48ded134daae06d0ab491be74a1fa4
Thorben Stangenberg <thorben@stangenberg.ch>
2015-07-11 12:23:24 +0200

    no dry run release

--- release.sh
+++ release.sh
@@ -3,10 +3,10 @@
 export COMMIT_MESSAGE=`git log -1 --pretty=%s`
 if  [[ $COMMIT_MESSAGE == "[maven-release-plugin] prepare release"* ]]
 then
-    mvn -s settings.xml release:perform > $CIRCLE_ARTIFACTS/perform_release.txt
+    mvn -s settings.xml -DdryRun=false release:perform > $CIRCLE_ARTIFACTS/perform_release.txt
 fi
 
 if  [[ $COMMIT_MESSAGE != [maven-release-plugin]* ]]
 then
-    mvn -s settings.xml --batch-mode release:prepare $CIRCLE_ARTIFACTS/prepare_release.txt
+    mvn -s settings.xml -DdryRun=false --batch-mode release:prepare > $CIRCLE_ARTIFACTS/prepare_release.txt
 fi
