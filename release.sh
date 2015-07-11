d6566b4538caca14e65579c85f1767ee19e8566d
Thorben Stangenberg <thorben@stangenberg.ch>
2015-07-11 12:04:32 +0200

    release script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export COMMIT_MESSAGE=`git log -1 --pretty=%s`
-if  [[ $COMMIT_MESSAGE == [maven-release-plugin] prepare release* ]]
+if  [[ $COMMIT_MESSAGE == "[maven-release-plugin] prepare release"* ]]
 then
     mvn -s settings.xml --batch-mode release:perform
 fi
